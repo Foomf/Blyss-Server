b966f76b6605d332f4f753da0faa48075fbfee08
Foomf <foomf@protonmail.com>
2019-11-20 19:00:39 -0800

    world stores a spawn location

--- include/world/world.hpp
+++ include/world/world.hpp
@@ -5,11 +5,14 @@
 #include <boost/uuid/uuid.hpp>
 
 #include "map.hpp"
+#include "cell_ref.hpp"
 
 namespace blyss::server::world
 {
     class world
     {
+        cell_ref spawn_location_{};
+
         std::map<boost::uuids::uuid, map> worlds_{};
 
     };
